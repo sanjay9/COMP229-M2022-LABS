8100ad058b0006cb6fc0b5c4acb7a9954facd06e
Sanjay <sanjaymahabir9@gmail.com>
2022-05-26 13:53:09 -0400

    converted lesson 2 to express and typescript

--- server.js
+++ server.js
@@ -1,39 +1,52 @@
-const http = require('http'); //CommonJS Module pattern (CJS)
-const fs = require('fs');
-const mime = require('mime-types');
-
-let lookup = mime.lookup; // alias for mime.lookup
-
-const port = 3000;
-
-const server = http.createServer(function(req, res) 
-{
-    let path = req.url; //alias fro req.url
-
-    if(path =="/" || path == "/home"){
-        path = "/index.html";
+#!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+const debug_1 = __importDefault(require("debug"));
+(0, debug_1.default)('lesson3:server');
+const http_1 = __importDefault(require("http"));
+const port = normalizePort(process.env.PORT || '3000');
+app_1.default.set('port', port);
+var server = http_1.default.createServer(app_1.default);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
     }
-
-    let mime_type = lookup(path.substring(1));
-    console.log(`MIME TYPE: ${mime_type}`);
-    //console.log(__dirname);
-    fs.readFile(__dirname + path, function(err, data)
-    {
-        if(err){
-            res.writeHead(404); // status - file not found
-            console.log(`ERROR: ${err.message}`);
-            return res.end(err.message); //output error message to the page
-            
-        }
-        res.setHeader("X-Content-Type-Options", "nosniff"); //security guard
-        res.writeHead(200, {'Content-Type': mime_type}); 
-       // res.writeHead(200); // status - all ok
-        res.end(data);
-        //console.log(`DATA: ${data}`);
-    });
-});
-
-server.listen(port, function()
-{
-    console.log(`Server running at Port: ${port}`);
-});
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+function onListening() {
+    let addr = server.address();
+    let bind = 'pipe ' + addr;
+    (0, debug_1.default)('Listening on ' + bind);
+}
+//# sourceMappingURL=server.js.map
